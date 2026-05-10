96e7938ead7cee660cf3395d9476791690fc06da
Katelyn Gigante <clockwork.singularity@gmail.com>
2026-05-10 15:23:55 +1000

    black

--- datapackage_validator.py
+++ datapackage_validator.py
@@ -9,10 +9,26 @@ import worlds
 
 print("\n")
 
-parser = argparse.ArgumentParser(description="Validate data package against known good file")
-parser.add_argument("input_file_path", help="Path to the input file", default=os.getenv("DATAPACKAGE_EXPORT_PATH"))
-parser.add_argument("game_name", help="Name of the game", default=os.getenv("DATAPACKAGE_EXPORT_GAME_NAME"))
-parser.add_argument("--update-datapackage", action="store_true", help="Update the datapackage export if validation is successful", default=os.getenv("DATAPACKAGE_EXPORT_UPDATE", "false").lower() in ("true", "1", "t"))
+parser = argparse.ArgumentParser(
+    description="Validate data package against known good file"
+)
+parser.add_argument(
+    "input_file_path",
+    help="Path to the input file",
+    default=os.getenv("DATAPACKAGE_EXPORT_PATH"),
+)
+parser.add_argument(
+    "game_name",
+    help="Name of the game",
+    default=os.getenv("DATAPACKAGE_EXPORT_GAME_NAME"),
+)
+parser.add_argument(
+    "--update-datapackage",
+    action="store_true",
+    help="Update the datapackage export if validation is successful",
+    default=os.getenv("DATAPACKAGE_EXPORT_UPDATE", "false").lower()
+    in ("true", "1", "t"),
+)
 args = parser.parse_args()
 
 input_file_path = args.input_file_path
@@ -32,7 +48,8 @@ if game_name not in worlds.network_data_package["games"].keys():
 known_datapackage = datapackage_export["games"][game_name]
 active_datapackage = worlds.network_data_package["games"][game_name]
 
-discrepancies : dict[str, dict[str,dict]] = {}
+discrepancies: dict[str, dict[str, dict]] = {}
+
 
 def validate_items(known_datapackage, active_datapackage, discrepancies):
     known_good_items = known_datapackage["item_name_to_id"]
@@ -40,14 +57,17 @@ def validate_items(known_datapackage, active_datapackage, discrepancies):
     # excluded_item_ids = []
     # checked_validation_items = validation_items.keys #list of validation keys, items removed when checking values against known good item list and remaining items are not found in known good items kno
 
-
     for item, id in known_good_items.items():
         if item not in validation_items.keys():
-            discrepancies[item] = {item : {"type" : "item", "known" : item, "validation" : None}}
+            discrepancies[item] = {
+                item: {"type": "item", "known": item, "validation": None}
+            }
         # excluded_item_ids.append(id)
         elif not validation_items[item] == id:
-        # checked_validation_items.pop(item)
-            discrepancies[item] = {item : {"type" : "id" ,"known" : id, "validation" : validation_items[item]}}
+            # checked_validation_items.pop(item)
+            discrepancies[item] = {
+                item: {"type": "id", "known": id, "validation": validation_items[item]}
+            }
             print(f"{item}: expected id: {id}, found id: {validation_items[item]}")
 
     # for id in excluded_item_ids:
@@ -59,33 +79,51 @@ def validate_items(known_datapackage, active_datapackage, discrepancies):
     #     if val["type"] == "id":
     #         print(f"{item}: {"known"} -> {"validation"}")
 
-
     # if not len(checked_validation_items) == 0:
     #     for item in checked_validation_items:
     #         discrepancies[item] = {item : {"type" : "item", "known" : None, "validation" : item}}
 
-def validate_locations(known_datapackage: worlds.DataPackage, active_datapackage: worlds.DataPackage, discrepancies: dict[str, dict[str,dict]]):
+
+def validate_locations(
+    known_datapackage: worlds.DataPackage,
+    active_datapackage: worlds.DataPackage,
+    discrepancies: dict[str, dict[str, dict]],
+):
     known_good_locations = known_datapackage["location_name_to_id"]
     validation_locations = active_datapackage["location_name_to_id"]
     for location, id in known_good_locations.items():
         if location not in validation_locations.keys():
-            discrepancies[location] = {location : {"type" : "location", "known" : location, "validation" : None}}
+            discrepancies[location] = {
+                location: {"type": "location", "known": location, "validation": None}
+            }
         # excluded_location_ids.append(id)
         elif not validation_locations[location] == id:
-        # checked_validation_locations.pop(location)
-            discrepancies[location] = {location : {"type" : "id" ,"known" : id, "validation" : validation_locations[location]}}
-            print(f"{location}: expected id: {id}, found id: {validation_locations[location]}")
+            # checked_validation_locations.pop(location)
+            discrepancies[location] = {
+                location: {
+                    "type": "id",
+                    "known": id,
+                    "validation": validation_locations[location],
+                }
+            }
+            print(
+                f"{location}: expected id: {id}, found id: {validation_locations[location]}"
+            )
 
     # for id in excluded_location_ids:
-        # if id in validation_locations.values():
-        #     discrepancies[id] = {id  : {"type" : "id reuse", "known" : id in known_good_locations}}
+    # if id in validation_locations.values():
+    #     discrepancies[id] = {id  : {"type" : "id reuse", "known" : id in known_good_locations}}
+
 
 validate_items(known_datapackage, active_datapackage, discrepancies)
 validate_locations(known_datapackage, active_datapackage, discrepancies)
 
 if len(discrepancies) == 0:
     print("Validation successful, no discrepancies found")
-    if update_datapackage and active_datapackage["checksum"] != known_datapackage["checksum"]:
+    if (
+        update_datapackage
+        and active_datapackage["checksum"] != known_datapackage["checksum"]
+    ):
         print("Updating datapackage export with current datapackage")
         datapackage_export["games"][game_name] = active_datapackage
         with open(input_file_path, "w") as f:
